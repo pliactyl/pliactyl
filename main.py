bb48ee3d7d1be9dfb573ed9b952f369829e7a69f
Vachan MN <vachanmn123@gmail.com>
2022-02-09 23:33:35 +0530

    basic(I mean basic) auth

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask
-from routes import index, login, logout
+from flask import Blueprint, render_template
+from . import db
+from flask_login import login_required, current_user
+
+main = Blueprint('main', __name__)
 
 
-app = Flask(__name__)
-app.secret_key = 'LADlskajhd37289roialkjnsx8p%^@&*$IEdba678923r'
+@main.route('/')
+def index():
+    return render_template('index.html')
 
-app.register_blueprint(index.index)
-app.register_blueprint(login.login)
-app.register_blueprint(logout.logout)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@login_required
+@main.route('/profile')
+def profile():
+    return render_template('profile.html', name=current_user.name)
